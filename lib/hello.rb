dd319aa1ebf59fe1025b55f17a133020405a40b9
lltchen <lltchen1@gmail.com>
2019-01-04 21:01:25 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,10 @@
+def hello_t(array)
+  i = 0
 
-def hello_t(["Tim", "Tom", "Jim"]) do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
+  while i < array.length
+    yield(array[i])
+    i = i + 1
   end
+
+  array
 end
